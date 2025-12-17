e8ef34dd439182ee134362c2fb016b7d97d91beb
vectoria-cloud <guilherme.horacio.cotia@gmail.com>
2025-12-17 14:12:45 -0300

    Delete SITE directory

--- SITE/static/javascript/js-header.js
+++
@@ -1,8 +0,0 @@
-var btnExp = document.querySelector('#btn-expand');
-var menu = document.querySelector('.menu-lateral');
-var content = document.querySelector('.content');
-
-btnExp.addEventListener('click', function(){
-    menu.classList.toggle('expandir');
-    content.classList.toggle('recall');
-})
